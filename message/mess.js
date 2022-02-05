4ed007584e2cbac932423d80edd8c0037608f68e
mellcanzz <kasu8273@gmail.com>
2022-02-05 17:33:25 +0700

    ramdandi

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'DONE',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER TOLOL',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'KHUSUS GRUP BANG',
+				admin: 'KHUSUS ADMIN BANG',
+				premium: 'LU BUKAN MEMBER PREMIUM,BELI DULU SANA',
+				owner: 'KHUSUS OWNER BANG',
+				Badmin: 'BOT HARUS JADI ADMIN BANG',
+			}
+		}
